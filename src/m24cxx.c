c4d7627c75b67cee6840e38f949d7fd6e7108471
Lars Boegild Thomsen <lth@netcompartner.com>
2023-07-17 11:13:36 +0800

    Started library

---
+++ src/m24cxx.c
@@ -0,0 +1,16 @@
+/**
+ ******************************************************************************
+ * @file           : m24cxx.c
+ * @brief          : M24Cxx Library Source
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2023 Lars Boegild Thomsen <lbthomsen@gmail.com>
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
---
+++ src/m24cxx.h
@@ -0,0 +1,23 @@
+/**
+ ******************************************************************************
+ * @file           : m24cxx.h
+ * @brief          : M24Cxx Library Header
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2023 Lars Boegild Thomsen <lbthomsen@gmail.com>
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+
+#ifndef M24CXX_H_
+#define M24CXX_H_
+
+
+
+#endif /* M24CXX_H_ */
